37df9f7973d3df1fe4918918a239de2d60f000c0
sen0rxol0 <sen0rxol0@users.noreply.github.com>
2022-10-13 16:20:07 +0200

    Update restore.sh

--- restore.sh
+++ restore.sh
@@ -23,6 +23,28 @@ then
   chmod 755 kairos/kairos
 fi
 
+if [ ! -e "/usr/local/bin/img4" ]; then
+  curl --progress-bar -o img4lib.tar.gz -L https://github.com/xerub/img4lib/releases/download/1.0/img4lib-2020-10-27.tar.gz
+  tar -xf img4lib.tar.gz
+  mv img4lib/apple/img4 /usr/local/bin/ && mv img4lib/apple/libimg4.a /usr/local/lib/;
+  rm -rf img4lib/ && rm img4lib.tar.gz;
+  chmod 755 /usr/local/bin/img4
+  xattr -d com.apple.quarantine /usr/local/bin/img4
+fi
+
+if [ ! -e "/usr/local/bin/img4tool" ]; then
+  curl --progress-bar -OL https://github.com/tihmstar/img4tool/releases/download/197/buildroot_macos-latest.zip
+  unzip -q buildroot_macos-latest.zip
+  usr_local=buildroot_macos-latest/usr/local
+  mv $usr_local/bin/img4tool /usr/local/bin/
+  mv $usr_local/include/img4tool /usr/local/include/
+  mv $usr_local/lib/libimg4tool.* /usr/local/lib/
+  mv $usr_local/lib/pkgconfig/libimg4tool.pc /usr/local/lib/pkgconfig/
+  rm -rf buildroot_macos-latest/ && rm buildroot_macos-latest.zip;
+  chmod 755 /usr/local/bin/img4tool
+  xattr -d com.apple.quarantine /usr/local/bin/img4tool
+fi
+
 mv futurerestore282 futurerestore
 xattr -d com.apple.quarantine irecovery 1> /dev/null
 xattr -d com.apple.quarantine futurerestore 1> /dev/null
