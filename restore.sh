16747be59b0babb7f8353c134f88c945540664a1
sen0rxol0 <sen0rxol0@users.noreply.github.com>
2022-10-13 15:39:00 +0200

    Update restore.sh

--- restore.sh
+++ restore.sh
@@ -1,20 +1,4 @@
 #!/bin/bash
-xattr -d com.apple.quarantine futurerestore
-chmod +x futurerestore
-chmod +x install.sh
-chmod +x main.sh
-cd files
-xattr -d com.apple.quarantine irecovery
-chmod +x irecovery
-cd ..
-echo "*** FutureRestore Booter ***"
-./main.sh
-clear
-echo "********* FutureRestore is Starting **********"
-sleep 5
-./install.sh
-
-#!/bin/bash
 
 if [ -z $2 ]
 then
