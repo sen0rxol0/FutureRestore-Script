8772b647991b7fd0096eaa815b1cb90a8f60a808
sen0rxol0 <sen0rxol0@users.noreply.github.com>
2022-10-19 15:22:04 +0200

    Update rerestore.sh

--- rerestore.sh
+++ rerestore.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
-echo "Waiting while device is being restored...."
+echo "Waiting while device is being restored ..."
 touch restore.log
-./futurerestore282 -t ./files/blob.shsh2 --latest-sep --latest-baseband $1 >restore.log 2>&1
+./futurerestore282 -t ./files/blob.shsh2 --latest-sep --latest-baseband $1 >restore.log 2>&1 &
+PID=$!
+i=0
+sp="/-\|"
+sleep 5
+printf " "
+while [ $(ps -p $PID | grep -c "$PID") != 0 ]
+do
+  printf "\b%s" "${sp:i++%${#sp}:1}"
+  sleep 0.1
+done
+printf "\n"
 
 if [ $(tail restore.log | grep -c "Status: Restore Finished") == 0 ]; then
   exit 1
