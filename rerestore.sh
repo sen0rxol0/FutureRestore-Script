4465bbe3c746eb7766037ec29d801e3e799b2283
sen0rxol0 <sen0rxol0@users.noreply.github.com>
2022-11-01 10:26:34 +0100

    Update rerestore.sh

--- rerestore.sh
+++ rerestore.sh
@@ -7,11 +7,13 @@ i=0
 sp="/-\|"
 sleep 5
 printf " "
+
 while [ $(ps -p $PID | grep -c "$PID") != 0 ]
 do
   printf "\b%s" "${sp:i++%${#sp}:1}"
   sleep 0.1
 done
+
 printf "\n"
 
 if [ $(tail restore.log | grep -c "Status: Restore Finished") == 0 ]; then
