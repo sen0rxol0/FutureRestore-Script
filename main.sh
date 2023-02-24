912e90df816da779d8037db3daad153192a9242b
sen0rxol0 <sen0rxol0@users.noreply.github.com>
2023-02-24 10:47:04 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -95,9 +95,10 @@ plutil -extract "ApImg4Ticket" xml1 -o - $shsh | xmllint -xpath '/plist/data/tex
 img4 -i ./files/ibss.patched -o ./files/ibss.img4 -A -M ./files/apticket.der -T ibss
 img4 -i ./files/ibec.patched -o ./files/ibec.img4 -A -M ./files/apticket.der -T ibec
 _print_yellow "Entering PWNREC mode"
-./gaster/gaster load ./files/ibss.img4 ./files/ibec.img4
-#./irecovery -f ./files/ibss.img4
-#./irecovery -f ./files/ibec.img4
+./gaster/gaster reset
+sleep 2
+./irecovery -f ./files/ibss.img4
+./irecovery -f ./files/ibec.img4
 
 #for did in iPhone9 iPhone10
 #do
